8ea6c3423c62bee050ead1ff5a098ea739735823
ivanhidalgo22 <ivanhidalgo22@gmail.com>
2024-09-16 21:10:29 -0500

    test

--- updateCheckPoint.py
+++ updateCheckPoint.py
@@ -1,14 +1,14 @@
 """
-This script queries the MongoDB database for the last checkpoint with the given last_activity and entity_type.
-author: Ivan Hidalgo
-date: 2024-09-09
+ Helper script to update check point in MongoDB
+ author: Ivan Hidalgo
+ date: 2024-09-09
+ 
+ python updateCheckPoint.py "REGISTERED" "tenant" "86a3710a-6357-482c-8896-1bdf1abce7ae"
+ 
+""" 
 
-python3 getCheckPoint.py COMPLETED tenant 
-
-"""
-
-import sys
 import os
+import sys
 import certifi
 from checkPoint import CheckPoint
 from checkPointRepository import CheckPointRepository
@@ -17,11 +17,11 @@ from pymongo import MongoClient
 
 DEFAULT_LAST_ACTIVITY = "REGISTERED"
 DEFAULT_ENTITY_TYPE = "tenant"
+DEFAULT_EXECUTION_ID = ""
 
-def query_mongodb(last_activity = "REGISTERED", entity_type = "tenant"):
+def query_mongodb(execution_id=DEFAULT_EXECUTION_ID, last_activity = DEFAULT_LAST_ACTIVITY, entity_type = DEFAULT_ENTITY_TYPE):
     
-    print("Fetching last check point in MongoDB")
-   
+    print("Updating check point in MongoDB")
     
     mongo_uri = os.getenv('MONGO_URI')
     if not mongo_uri:
@@ -42,10 +42,16 @@ def query_mongodb(last_activity = "REGISTERED", entity_type = "tenant"):
     
     
     checkpoint_repository = CheckPointRepository(db)
-    retrieve_checkpoints = list(checkpoint_repository.find_with_filter({"last_activity": last_activity, "entity_type": entity_type}))
+    retrieve_checkpoints = list(checkpoint_repository.find_with_filter({"entity_type": entity_type, "execution_id": execution_id}))
+    print(f"Retrieved {checkpoint_repository.count()} checkpoints with last_activity: {last_activity} and entity_type: {entity_type} and execution_id: {execution_id}")
     if len(retrieve_checkpoints) == 0:
-        print(f"No checkpoints found with last_activity: {last_activity} and entity_type: {entity_type}")
+        print(f"No checkpoints found with last_activity: {last_activity} and entity_type: {entity_type} and execution_id: {execution_id}")
         return
+    documentToBeUpdated = retrieve_checkpoints[0]
+    documentToBeUpdated['previous_activity'] = documentToBeUpdated['last_activity']
+    documentToBeUpdated['last_activity'] = last_activity
+    print(f"Updating document with last_activity: {documentToBeUpdated['last_activity']} and previous_activity: {documentToBeUpdated['previous_activity']}")
+    checkpoint_repository.update_one(documentToBeUpdated['execution_id'], documentToBeUpdated)
     
     documents = retrieve_checkpoints
     for doc in documents:
@@ -54,7 +60,7 @@ def query_mongodb(last_activity = "REGISTERED", entity_type = "tenant"):
 if __name__ == "__main__":
     load_dotenv()
     
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 4:
         if sys.argv[1] is not None:
             last_activity = sys.argv[1]
         else:
@@ -63,10 +69,11 @@ if __name__ == "__main__":
             entity_type = sys.argv[2]
         else:
             entity_type = DEFAULT_ENTITY_TYPE
-    else:
-        last_activity = DEFAULT_LAST_ACTIVITY
-        entity_type = DEFAULT_ENTITY_TYPE
+        if sys.argv[3] is not None:
+            execution_id = sys.argv[3]
+        else:
+            execution_id = DEFAULT_EXECUTION_ID
     
-    print(f"Querying MongoDB with last_activity: {last_activity} and entity_type: {entity_type}")
+    print(f"Updating check point with last_activity: {last_activity} and entity_type: {entity_type} and execution_id: {execution_id}")
     
-    query_mongodb(last_activity, entity_type)
+    query_mongodb(execution_id, last_activity, entity_type)
